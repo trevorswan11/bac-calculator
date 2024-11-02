42778886c5a5ae31c88e8edba1f66925de325029
Trevor Swan <tcs94@case.edu>
2024-11-01 20:48:59 -0400

    exe

---
+++ target/release/deps/bac_calculator.d
@@ -0,0 +1,5 @@
+C:\Users\Trevor\bac-calculator\target\release\deps\bac_calculator.exe: src/main.rs
+
+C:\Users\Trevor\bac-calculator\target\release\deps\bac_calculator.d: src/main.rs
+
+src/main.rs:
